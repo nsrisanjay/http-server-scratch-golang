a57db4f239b96e96313fb44d5bb283b7f14d9004
sanjay <nsrisanjay03@gmail.com>
2025-08-21 17:47:06 +0530

    HTTP REQUESTLINE PARSER

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,67 +1,75 @@
 package main
 
 import (
-	"bytes"
+	// "bytes"
 	"fmt"
-	"io"
+	"httpFromScratch/internal/request"
+	// "io"
 	"log"
 	"net"
 )
 
-func getLinesChannel(f io.ReadCloser)<-chan string{
-	// make a channel of strings
-	ch := make(chan string,1)
-	go func() {
-		defer close(ch)
-		defer f.Close()
-		// read the text in message.txt 8 byte at a time
-		str := ""
-		for{
-			data := make([]byte,8)
-			n,err := f.Read(data)
-			if err!=nil{
-				break
-			}
-			// n represents the number of bytes read could be less than 8 or 8
-			data = data[:n]
-			// return the index at which '\n' lies in the byte of data
-			index := bytes.IndexByte(data,'\n')
-			if index!=-1 {
-				str += string(data[:index])
-				// now save data after the '\n'
-				data = data[index+1:]
-				// fmt.Printf("read : %s\n",str)
-				ch<-str
-				str = ""
-			}
-			// append the saved data
-			str += string(data)
-		}
-		if len(str)!=0{
-			// fmt.Printf("file: %s\n",str)
-			ch<-str
-		}
-	}()
-	return ch
-}
+// func getLinesChannel(f io.ReadCloser)<-chan string{
+// 	// make a channel of strings
+// 	ch := make(chan string,1)
+// 	go func() {
+// 		defer close(ch)
+// 		defer f.Close()
+// 		// read the text in message.txt 8 byte at a time
+// 		str := ""
+// 		for{
+// 			data := make([]byte,8)
+// 			n,err := f.Read(data)
+// 			// n represents the number of bytes read could be less than 8 or 8
+// 			if err!=nil{
+// 				break
+// 			}
+// 			data = data[:n]
+// 			// return the index at which '\n' lies in the byte of data
+// 			index := bytes.IndexByte(data,'\n')
+// 			if index!=-1 {
+// 				str += string(data[:index])
+// 				// now save data after the '\n'
+// 				data = data[index+1:]
+// 				// fmt.Printf("read : %s\n",str)
+// 				ch<-str
+// 				str = ""
+// 			}
+// 			// append the saved data
+// 			str += string(data)
+// 		}
+// 		if len(str)!=0{
+// 			// fmt.Printf("file: %s\n",str)
+// 			ch<-str
+// 		}
+// 	}()
+// 	return ch
+// }
 
 func main(){
-	fmt.Println("I hope i get that job!")
+	//fmt.Println("I hope i get that job!")
 	// create a tcp listener on port 42069
 	listener,err := net.Listen("tcp",":42069")
 	if err!=nil{
-		log.Fatal("Error",err)
+		log.Fatalf("Error: %v",err)
 	}
 	for{
 		conn,err := listener.Accept()
 		if err!=nil{
 			log.Fatal("error: ",err)
 		}
-		fmt.Println("TCP connection has been accepted")
-		lines := getLinesChannel(conn)
-		for line := range lines{
-			fmt.Printf("%s\n",line)
-		}
-		fmt.Println("TCP connection has been closed")
+		//fmt.Println("TCP connection has been accepted")
+		// lines := getLinesChannel(conn)
+		// for line := range lines{
+		// 	fmt.Printf("%s\n",line)
+		// }
+		//fmt.Println("TCP connection has been closed")
+
+		// use the requstReader function to process the request line
+		req,err := request.RequestFromReader(conn)
+		if err!=nil{
+			log.Fatal("error",err)
+		} 
+		fmt.Printf("Method: %s\n Target: %s\n Version: %s\n",req.RequestLine.Method,req.RequestLine.RequestTarget,req.RequestLine.HttpVersion)
 	}
 }
---
+++ internal/request/request.go
@@ -0,0 +1,135 @@
+package request
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+)
+
+// what we are doing in this file of code
+
+// When we read,
+// all we're doing is moving the data from the reader (which in the case of HTTP is a network connection,
+// but it could be a file as well, our code is agnostic) into our program. When we parse,
+// we're taking that data and interpreting it (moving it from a []byte to a
+// RequestLine struct). Once its parsed, we can discard it from the buffer to save memory.
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+type parserState string
+const(
+	StateInit parserState = "init"
+	StateDone parserState = "done"
+	StateError parserState = "Error"
+)
+
+type Request struct {
+	RequestLine RequestLine
+	state parserState
+}
+
+func newRequest() *Request{
+	return &Request{
+		state: StateInit,
+	}
+}
+
+var ERROR_MALFORMED_REQUEST_LINE = fmt.Errorf("malformed request line")
+var ERROR_UNSUPPORTED_HTTP_VERSION = fmt.Errorf("unsupported HTTP version")
+var ERROR_REQUEST_IN_ERROR_STATE = fmt.Errorf("Request in error state")
+var SEPARATOR =[]byte("\r\n")
+
+// func (r *RequestLine) ValidHTTP() bool{
+// 	return r.HttpVersion == "HTTP/1.1"
+// }
+
+func parseRequestLine(b []byte)(*RequestLine,int,error){
+	// fmt.Printf("hello======================================")
+	idx := bytes.Index(b,SEPARATOR)
+	if( idx==-1){
+		fmt.Printf("idx = -1")
+		return nil,0,nil
+	}
+	startLine := b[:idx]
+	// fmt.Printf("THis is start line------------------%s",string(startLine))
+	read := idx+len(SEPARATOR)
+	parts := bytes.Split(startLine,[]byte(" "))
+	if len(parts)!=3{
+		return nil,0,ERROR_MALFORMED_REQUEST_LINE
+	}
+	httpParts := bytes.Split(parts[2],[]byte("/"))
+	if len(httpParts) != 2 || string(httpParts[0])!="HTTP" || string(httpParts[1])!="1.1"{
+		return nil,0,ERROR_UNSUPPORTED_HTTP_VERSION
+	}
+	rl := &RequestLine{
+		Method: string(parts[0]),
+		RequestTarget: string(parts[1]),
+		HttpVersion: string(httpParts[1]),
+	}
+	return rl,read,nil
+}
+func (r *Request) parse(data []byte) (int,error){
+	read:=0
+outer:
+	for{
+		switch r.state{
+		case StateError:
+			return 0,ERROR_REQUEST_IN_ERROR_STATE
+		case StateInit:
+			// we need to parse header
+			rl,n,err := parseRequestLine(data[read:])
+			if err!=nil{
+				r.state = StateError
+				return 0,err
+			}
+			if n ==0{
+				break outer
+			} 
+			r.RequestLine = *rl
+			read += n
+
+			r.state = StateDone
+		case StateDone:
+			break outer
+		}
+	}
+	return read,nil
+}
+func (r *Request) done() bool{
+	return r.state == StateDone || r.state == StateError
+}
+
+func RequestFromReader(reader io.Reader)(*Request,error){
+	request := newRequest()
+	buf := make([]byte,1024)
+	bufLen := 0
+	for !request.done(){
+		// read from network stream
+		n,err := reader.Read(buf[bufLen:])
+		// fmt.Printf("DEBUG raw line: %q\n", string(buf[:n]))
+		if err!=nil{
+			if errors.Is(err,io.EOF){
+				request.state=StateDone
+				break
+			}
+			return nil,err
+		}
+		// add not of bytes Read to bufLen
+		bufLen += n
+
+		readN,err := request.parse(buf[:bufLen])
+		// after parsing some of the data in buffer
+		// change buffer to un-parsed data
+
+		// readN of buffer gets parsed, so the remaining readN to bufLen remains unparsed
+		// hence update buf with it.
+		copy(buf,buf[readN:bufLen])
+		bufLen -= readN
+	}
+	return request,nil;
+}
